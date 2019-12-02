1f314e3cdc8c4d3a92bd46b3cae300a174f3a1b2
Zoe <zcahill90@gmail.com>
2019-12-02 23:18:05 +0000

    started adding db functions in moviesDAO and altering server, up to update function

---
+++ movieDAO.py
@@ -0,0 +1,79 @@
+import mysql.connector
+class MovieDAO:
+    db=""
+    def __init__(self): 
+        self.db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="project"
+        )
+
+
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into movies (name,genre,description,totalVotes) values (%s,%s,%s,%s)"
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        return cursor.lastrowid
+
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from movies"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+
+        return returnArray
+
+    def get_movie(self, name):
+        cursor = self.db.cursor()
+        sql="select * from movies where name = %s"
+        values = (name,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql="select * from movies where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def update_movie(self, values):
+        cursor = self.db.cursor()
+        sql="update movies set name= %s,genre=%s, description=%s, totalVotes=%s where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from book where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['id','Title','Author', "Price"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
+movieDAO = MovieDAO()
--- simpleserver.py
+++ simpleserver.py
@@ -1,94 +1,172 @@
 #!flask/bin/python
 from flask import Flask, jsonify,  request, abort, make_response
+from movieDAO import movieDAO
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='.')
 
 # JSON where inital movies are coming from
-movies = [
-    {
-        "id": 0,
-        "name":"Avengers: Endgame",
-        "genre":"Action & Adventure",
-        "description":"The grave course of events set in motion by Thanos that wiped out half the universe and fractured the Avengers ranks compels the remaining Avengers to take one final stand in Marvel Studios' grand conclusion to twenty-two films, Avengers: Endgame.",
-        "totalVotes":9
-    },
-    {
-        "id": 1,
-        "name":"The Wizard of Oz",
-        "genre":"Classics",
-        "description":"L. Frank Baum's classic tale comes to magisterial Technicolor life! The Wizard of Oz stars legendary Judy Garland as Dorothy, an innocent farm girl whisked out of her mundane earthbound existence into a land of pure imagination. Dorothy's journey in Oz will take her through emerald forests, yellow brick roads, and creepy castles, all with the help of some unusual but earnest song-happy friends",
-        "totalVotes":2
-    },
-    {
-        "id": 2,
-        "name":"Dunkirk",
-        "genre":"Drama",
-        "description":"Acclaimed auteur Christopher Nolan directs this World War II thriller about the evacuation of Allied troops from the French city of Dunkirk before Nazi forces can take hold. Tom Hardy, Kenneth Branagh and Mark Rylance co-star, with longtime Nolan collaborator Hans Zimmer providing the score.",
-        "totalVotes":6
-    }
-]
+#movies = [
+ #   {
+  #      "id": 0,
+   #     "name":"Avengers: Endgame",
+    #    "genre":"Action & Adventure",
+    #    "description":"The grave course of events set in motion by Thanos that wiped out half the universe and fractured the Avengers ranks compels the remaining Avengers to take one final stand in Marvel Studios' grand conclusion to twenty-two films, Avengers: Endgame.",
+    #    "totalVotes":9
+    #},
+    #{
+    #    "id": 1,
+    #    "name":"The Wizard of Oz",
+    #    "genre":"Classics",
+    #    "description":"L. Frank Baum's classic tale comes to magisterial Technicolor life! The Wizard of Oz stars legendary Judy Garland as Dorothy, an innocent farm girl whisked out of her mundane earthbound existence into a land of pure imagination. Dorothy's journey in Oz will take her through emerald forests, yellow brick roads, and creepy castles, all with the help of some unusual but earnest song-happy friends",
+    #    "totalVotes":2
+    #},
+    #{
+    #    "id": 2,
+    #    "name":"Dunkirk",
+    #    "genre":"Drama",
+    #    "description":"Acclaimed auteur Christopher Nolan directs this World War II thriller about the evacuation of Allied troops from the French city of Dunkirk before Nazi forces can take hold. Tom Hardy, Kenneth Branagh and Mark Rylance co-star, with longtime Nolan collaborator Hans Zimmer providing the score.",
+    #    "totalVotes":6
+    #}
+#]
 
-nextId=3
+#nextId=3
 
-@app.route('/movies', methods=['GET'])
+
+#curl "http://127.0.0.1:5000/movies"
+@app.route('/movies')
 def get_movies():
-    return jsonify( {'movies' : movies})
+    #print("in getall")
+    results = movieDAO.getAll()
+    return jsonify(results)
 
-@app.route('/movies/<string:name>', methods =['GET'])
+
+#@app.route('/movies', methods=['GET'])
+#def get_movies():
+#    return jsonify( {'movies' : movies})
+
+#curl "http://127.0.0.1:5000/movies/2"
+@app.route('/movies/<string:name>')
 def get_movie(name):
-    foundmovies = list(filter(lambda t : t['name'] == name , movies))
-    if len(foundmovies) == 0:
-        return jsonify( { 'movie' : '' }),204
-    return jsonify( { 'movie' : foundmovies[0] })
+    foundmovies = movieDAO.get_movie(name)
+
+    return jsonify(foundmovies)
+
+#@app.route('/movies/<string:name>', methods =['GET'])
+#def get_movie(name):
+#    foundmovies = list(filter(lambda t : t['name'] == name , movies))
+#    if len(foundmovies) == 0:
+#        return jsonify( { 'movie' : '' }),204
+#    return jsonify( { 'movie' : foundmovies[0] })
     
     
+#curl "http://127.0.0.1:5000/movies/2"
 @app.route('/movies/<int:id>')
 def findById(id):
-    foundmovies = list(filter(lambda t: t['id'] == id, movies))
-    if len(foundmovies) == 0:
-        return jsonify ({}) , 204
+    foundmovies = movieDAO.findByID(id)
 
-    return jsonify(foundmovies[0])
+    return jsonify(foundmovies)
 
+#@app.route('/movies/<int:id>')
+#def findById(id):
+#   foundmovies = list(filter(lambda t: t['id'] == id, movies))
+#     if len(foundmovies) == 0:
+#        return jsonify ({}) , 204
+
+#    return jsonify(foundmovies[0])
+
+#curl  -i -H "Content-Type:application/json" -X POST -d "{\"name\":\"hello\",\"genre\":\"Drama\",\"description\":\"hello\",\"totalVotes\":123}" http://127.0.0.1:5000/movies
 @app.route('/movies', methods=['POST'])
-def create_movie():
-    global nextId
+def create():
+    
     if not request.json:
         abort(400)
-    if not 'name' in request.json:
-        abort(400)
-    movie={
-        "id": nextId,
+    # other checking 
+    movie = {
         "name":  request.json['name'],
         "genre": request.json['genre'],
         "description":request.json['description'],
         "totalVotes":0
     }
+    values =(movie['name'],movie['genre'],movie['description'],movie['totalVotes'])
+    newId = movieDAO.create(values)
+    movie['id'] = newId
+    return jsonify(movie)
+
+
+#@app.route('/movies', methods=['POST'])
+#def create_movie():
+#    global nextId
+#    if not request.json:
+#        abort(400)
+#    if not 'name' in request.json:
+#        abort(400)
+#    movie={
+#        "id": nextId,
+#        "name":  request.json['name'],
+#        "genre": request.json['genre'],
+#        "description":request.json['description'],
+#        "totalVotes":0
+#    }
     
-    nextId+=1
-    movies.append(movie)
-    return jsonify({'movie':movie }),201
+#    nextId+=1
+#    movies.append(movie)
+#    return jsonify({'movie':movie }),201
 
 
-@app.route('/movies/<string:name>', methods =['PUT'])
+
+
+#curl  -i -H "Content-Type:application/json" -X PUT -d "{\"name\":\"another\",\"genre\":\"Comedy\",\"description\":\"new\",\"totalVotes\":11}" http://127.0.0.1:5000/movies/hello
+@app.route('/movies/<string:name>', methods=['PUT'])
 def update_movie(name):
-    foundmovies=list(filter(lambda t : t['name'] ==name, movies))
-    if len(foundmovies) == 0:
+    foundmovie = movieDAO.get_movie(name)
+    print(foundmovie)
+    if not foundmovie:
+        print(name)
+        print('no mmmooovie')
         abort(404)
+    
     if not request.json:
         abort(400)
-    if 'genre' in request.json and type(request.json['genre']) != str:
+    reqJson = request.json
+    if 'totalVotes' in reqJson and type(reqJson['totalVotes']) is not int:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not str:
-        abort(400)
-    if 'totalVotes' in request.json and type(request.json['totalVotes']) is not int:
-        abort(400)
-    foundmovies[0]['genre']  = request.json.get('genre', foundmovies[0]['genre'])
-    foundmovies[0]['description'] =request.json.get('description', foundmovies[0]['description'])
-    foundmovies[0]['totalVotes'] =request.json.get('totalVotes', foundmovies[0]['totalVotes'])
-    return jsonify( {'movie':foundmovies[0]})
+
+    #if 'id' in reqJson:
+     #   foundmovie['id'] = reqJson['id']
+    if 'name' in reqJson:
+        foundmovie['name'] = reqJson['name']
+    if 'genre' in reqJson:
+        foundmovie['genre'] = reqJson['genre']
+    if 'description' in reqJson:
+        foundmovie['description'] = reqJson['description']
+    if 'totalVotes' in reqJson:
+        foundmovie['totalVotes'] = reqJson['totalVotes']
+    values = (foundmovie['name'],foundmovie['genre'],foundmovie['description'],foundmovie['totalVotes'],foundmovie['id'])
+    movieDAO.update_movie(values)
+    return jsonify(foundmovie)
+
+
+
+
+#@app.route('/movies/<string:name>', methods =['PUT'])
+#def update_movie(name):
+#    foundmovies=list(filter(lambda t : t['name'] ==name, movies))
+#    if len(foundmovies) == 0:
+#        abort(404)
+#    if not request.json:
+#        abort(400)
+#    if 'genre' in request.json and type(request.json['genre']) != str:
+#        abort(400)
+#    if 'description' in request.json and type(request.json['description']) is not str:
+#        abort(400)
+#    if 'totalVotes' in request.json and type(request.json['totalVotes']) is not int:
+#        abort(400)
+#    foundmovies[0]['genre']  = request.json.get('genre', foundmovies[0]['genre'])
+#    foundmovies[0]['description'] =request.json.get('description', foundmovies[0]['description'])
+#    foundmovies[0]['totalVotes'] =request.json.get('totalVotes', foundmovies[0]['totalVotes'])
+#    return jsonify( {'movie':foundmovies[0]})
 
     
 @app.route('/votes/<int:movieid>', methods = ['POST'])
